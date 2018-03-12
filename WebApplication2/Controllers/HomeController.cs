14527fe2ef20881d275db0a9f2e479427b340e9b
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-03-13 02:41:57 +0300

    added simple websockets chat

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("MainChat");
+        }
+
+        [HttpPost]
+        public IActionResult Index(ChatUser n)
+        {
+            return View("Index", ChatUser.name);
+        }
+
+        public IActionResult goToChat()
+        {
+            return View("Index", ChatUser.name);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
